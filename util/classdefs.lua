e9a147714f15f685610cfdae1b6632b10e246ec7
Alan Litz <alanjlitz@gmail.com>
2022-02-10 23:57:14 -0500

    add class defs for type hinting

---
+++ util/classdefs.lua
@@ -0,0 +1,21 @@
+
+---@class Player
+---@field steam_id string
+---@field peer_id number
+---@field name string
+---@field is_admin boolean
+---@field is_auth boolean
+
+---@class Vehicle
+---@field peer_id number
+---@field spawn_coords Coordinates
+---@field cost number
+---@field loaded boolean
+---@field mass number
+---@field voxels number
+---@field filename string
+
+---@class Coordinates
+---@field x number
+---@field y number
+---@field z number
